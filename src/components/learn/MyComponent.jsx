b51ae0cbb9a6295b288c3794725d50dacca7b62b
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2024-11-24 22:34:48 +0700

    JSX variable

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,19 @@ import './style.css'
 
 // component = html + css + js
 const MyComponent = () => {
+    // const name = "Thanh"; //string
+    // const name = 123; //number
+    // const name = true; //boolean
+    // const name = undefined;
+    // const name = null; 
+    const name = [1, 2, 3];
+    // const people = {
+    //     name: "Thanh",
+    //     age: 20
+    // }
     return (
         <>
-      <div> hello component </div>
+      <div> hello {JSON.stringify(name)} </div>
       <div className="child"
         style={{borderRadius: "10px"}}
       >child</div>
