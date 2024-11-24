d5336e6c30ce0d55b27f9415b2ee09599d13f627
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2024-11-24 22:16:22 +0700

    JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,14 @@
+import './style.css'
+
 // component = html + css + js
 const MyComponent = () => {
     return (
+        <>
       <div> hello component </div>
+      <div className="child"
+        style={{borderRadius: "10px"}}
+      >child</div>
+      </>
     );
   }
   export default MyComponent;
