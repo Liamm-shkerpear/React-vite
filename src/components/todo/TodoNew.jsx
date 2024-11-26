fcb8a7a164bc5e5b3d5e05d7d73e7cb7c5ad8b2b
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2024-11-26 22:43:29 +0700

    handle event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,10 +3,21 @@ const TodoNew = (props) => {
     const {addNewTodo} = props;
 
 //    addNewTodo("Thanh");
+    const handleClick = () => {
+        alert("Click me")
+    }
+    const handleOnChange = (name) => {
+        console.log(">>>>>Handle on change", name)
+    }
     return (
     <div className='todo-input'>
-      <input type="text"/>
-      <button>Add</button>
+      <input type="text"
+      onChange={(event) => handleOnChange(event.target.value)}
+      />
+      <button
+       style={{cursor: "pointer"}} 
+       onClick={handleClick}
+       >Add</button>
      </div>
     );
 }
